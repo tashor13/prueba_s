85760216f23f95de580afa892a7c2d9411e23561
tashor13 <davidsaracay@gmail.com>
2025-06-23 17:35:23 -0500

    examen

--- app/src/main/java/com/example/api_web_json/LugaresTuristicos_Activity.java
+++ app/src/main/java/com/example/api_web_json/LugaresTuristicos_Activity.java
@@ -19,48 +19,23 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.BreakIterator;
 import java.util.HashMap;
 import java.util.Map;
 
 public class LugaresTuristicos_Activity extends AppCompatActivity implements Asynchtask {
 
+    Bundle bundle = this.getIntent().getExtras();
+    Map<String, String> datos = new HashMap<String, String>();
+    WebService ws= new WebService(
+            "https://uteqia.com/api/choferes"
+                    + bundle.getString("Usr") + "&pass=" + bundle.getString("clave"),
+            datos, LugaresTuristicos_Activity.this, LugaresTuristicos_Activity.this);
+ws.execute("GET");
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_lugares_turisticos);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-        AutoCompleteTextView autoListaLugares =
-                findViewById(R.id.autoListaLugares);
-                autoListaLugares.setOnItemClickListener((AdapterView<?>parent,
-                                                         View view, int position, long id) -> {
-        Map<String, String> datos = new HashMap<String, String>();
-        WebService ws= new WebService(
-                "https://turismo.quevedoenlinea.gob.ec/lugar_turistico/json_getlistadoGridLT/"+ (position+1)
-        , datos, LugaresTuristicos_Activity.this, LugaresTuristicos_Activity.this);
-        ws.execute("GET");
-    });
+    public void processFinish(String result) throws
+            JSONException {
+        BreakIterator txtResp= null;
+        txtResp.setText("Resp: " + result );
     }
-    @Override
-    public void processFinish(String result) throws JSONException {
-        TextView txtSaludo=findViewById(R.id.TxtResp);
-        String lstLista="";
-        JSONObject resultados =  new JSONObject(result);
-        JSONArray JSONlista =  resultados.getJSONArray("data");
-        for(int i=0; i< JSONlista.length();i++){
-            JSONObject banco=  JSONlista.getJSONObject(i);
-            lstLista = lstLista + i + ".- " +
-                    banco.getString("nombre_lugar").toString()
-                    + " - " +
-                    banco.getString("categoria").toString() + "\n" ;
-
-        }
-
-        txtSaludo.setText(lstLista );
-    }
-
 }
